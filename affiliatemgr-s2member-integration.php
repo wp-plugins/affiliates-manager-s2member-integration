0002638877df496c77e4a55a17540cd6dca5a175
affmngr <affmngr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-31 23:14:52 +0000

    .first commit
    git-svn-id: https://plugins.svn.wordpress.org/affiliates-manager-s2member-integration/trunk@1057683 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliatemgr-s2member-integration.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+  Plugin Name: Affiliates Manager S2Member Integration
+  Plugin URI: https://wpaffiliatemanager.com
+  Description: Process an affiliate commission via Affiliates Manager after a S2Member payment.
+  Version: 1.0
+  Author: wp.insider, affmngr
+  Author URI: https://wpaffiliatemanager.com
+ */
+
+if (defined("WS_PLUGIN__S2MEMBER_VERSION")) {
+    add_action("ws_plugin__s2member_before_sc_paypal_button_after_shortcode_atts", "wpam_s2member_integration");
+    add_action("ws_plugin__s2member_pro_before_sc_paypal_form_after_shortcode_atts", "wpam_s2member_integration");
+    add_action("ws_plugin__s2member_pro_before_sc_authnet_form_after_shortcode_atts", "wpam_s2member_integration");
+    add_action("plugins_loaded", "wpam_s2member_notification_url");
+    add_action("init", "wpam_s2member_process_affiliate_commission");
+}
+
+function wpam_s2member_integration($vars = array()) {
+    if (isset($_COOKIE[WPAM_PluginConfig::$RefKey])) {
+        $strRefKey = $_COOKIE[WPAM_PluginConfig::$RefKey];
+        $vars["__refs"]["attr"]["custom"] .= "|" . $strRefKey;
+    }
+}
+
+function wpam_s2member_notification_url() {
+    $urls = &$GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["payment_notification_urls"];
+    $notification_url = home_url() . '/?wpam_s2member=1&refkey=%%cv1%%&sale_amt=%%amount%%&txn_id=%%txn_id%%';
+    $pos = strpos($urls, $notification_url);
+    if ($pos === false) {
+        $urls = trim($urls . "\n" . $notification_url);
+    }
+
+    $specific_post_page_urls = &$GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["sp_sale_notification_urls"];
+    $pos2 = strpos($specific_post_page_urls, $notification_url);
+    if ($pos2 === false) {
+        $specific_post_page_urls = trim($specific_post_page_urls . "\n" . $notification_url);
+    }
+}
+
+function wpam_s2member_process_affiliate_commission() {
+    if (isset($_REQUEST['wpam_s2member']) && isset($_REQUEST['refkey'])) {
+        WPAM_Logger::log_debug('S2Member Integration - notification received.');
+        $strRefKey = strip_tags($_REQUEST['refkey']);
+        if (!empty($strRefKey)) {
+            WPAM_Logger::log_debug('S2Member Integration - Tracking data present. Need to track affiliate commission. Tracking value: ' . $strRefKey);
+            $sale_amt = strip_tags($_REQUEST['sale_amt']);
+            $txn_id = strip_tags($_REQUEST['txn_id']);
+            $requestTracker = new WPAM_Tracking_RequestTracker();
+            $requestTracker->handleCheckoutWithRefKey($txn_id, $sale_amt, $strRefKey);
+            WPAM_Logger::log_debug('S2Member Integration - Commission tracked for transaction ID: ' . $txn_id . '. Purchase amt: ' . $sale_amt);
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Affiliates Manager S2Member Integration ===
+Contributors: wp.insider, affmngr
+Donate link: https://wpaffiliatemanager.com/
+Tags: s2 member, membership, affiliate, affiliates, users, affiliates manager, integration
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2 or later
+
+Process an affiliate commission via Affiliates Manager after a S2Member payment
+
+== Description ==
+
+This addon allows you to integrate S2Member plugin with the Affiliates Manager plugin.
+
+When members pay for membership on your site, this addon will check to see if the member was referred to your site by an affiliate. It will then give commission to the affiliate who referred the user.
+
+This addon requires the [Affiliates Manager Plugin](https://wordpress.org/plugins/affiliates-manager/).
+
+Read the usage documentation [here](https://wpaffiliatemanager.com/affiliates-manager-s2member-integration/).
+
+== Installation ==
+
+1. Upload `affiliates-manager-s2member-integration` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+None
+
+== Screenshots ==
+Check the following page for screenshots
+https://wpaffiliatemanager.com/
+
+== Changelog ==
+
+= 1.0 =
+* First commit to wordpress.org
+
+== Upgrade Notice ==
+None
